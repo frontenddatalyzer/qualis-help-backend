e0615464ee2a7469dfc825fe488402047a1eee2a
Narasimha Murthy K <narasimha.k@datalyzerint.com>
2025-06-26 13:18:16 +0530

    spc node file updated

--- src/api/spc-node/routes/spc-node.ts
+++ src/api/spc-node/routes/spc-node.ts
@@ -4,4 +4,13 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::spc-node.spc-node');
+export default factories.createCoreRouter('api::spc-node.spc-node',{
+    config: {
+        find:{
+            middlewares: ["api::spc-node.spc-nodes-populate"]
+        },
+         findOne:{
+            middlewares: ["api::spc-node.spc-nodes-populate"]
+        }
+    }
+});
